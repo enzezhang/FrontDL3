caded7a95478e6c9df468924a34924f32c4ef9b8
EnzeZHANG <zhangenze@link.cuhk.edu.hk>
2021-08-16 14:27:48 -0500

    update

--- build_folder_structure.sh
+++ build_folder_structure.sh
@@ -1,42 +1,56 @@
 #!/bin/bash
 path=$1
 cd $path
-dir=(`ls |grep "GID"`)
-i=0
-count=${#dir[@]}
-while(($i<$count))
+dir1=(`ls |grep "GID"`)
+j=0
+count_j=${#dir1[@]}
+while (($j<$count_j))
 do
-	cd ${dir[i]}
-	pwd
-	if [ -d original_image ];then
-		echo "original_image exist"
+	cd ${dir1[j]}
+	dir=(`ls |grep "GID"`)
+	if [ -d polygon ];then
+		echo "polygon exish"
 	else
-		echo "mkdir original_image"
-		mkdir original_image
-		echo "mv *.tif original_image"
-		mv *.tif original_image
-		
+		mkdir polygon
 	fi
-	if [ -d script ];then
-		echo "script exist"
-	else
-		mkdir script
-	fi
-	if [ -d hist_eq ];then
-                echo "hist_eq exist"
-        else
-                mkdir hist_eq
-        fi
-	if [ -d in_polygon_gmt ];then
-                echo "in_polygon_gmt exist"
-        else
-                mkdir in_polygon_gmt
-        fi
-	if [ -d figure ];then
-                echo "figure exist"
-        else
-                mkdir figure
-        fi
+	i=0
+	count=${#dir[@]}
+	while(($i<$count))
+	do
+		cd ${dir[i]}
+		pwd
+		if [ -d original_image ];then
+			echo "original_image exist"
+		else
+			echo "mkdir original_image"
+			mkdir original_image
+			echo "mv *.tif original_image"
+			rm *.xml
+			mv *.tif original_image		
+		fi
+		if [ -d script ];then
+			echo "script exist"
+		else
+			mkdir script
+		fi
+		if [ -d hist_eq ];then
+                	echo "hist_eq exist"
+        	else
+                	mkdir hist_eq
+        	fi
+		if [ -d in_polygon_gmt ];then
+                	echo "in_polygon_gmt exist"
+        	else
+                	mkdir in_polygon_gmt
+        	fi
+		if [ -d figure ];then
+                	echo "figure exist"
+        	else
+                	mkdir figure
+        	fi
+		cd ../
+		i=$[i+1]
+	done
 	cd ../
-	i=$[i+1]
+	j=$[j+1]	
 done
